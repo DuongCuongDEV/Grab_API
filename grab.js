05354980cd15e413b2a538c1ff33b5cfb58efa0a
Giap Van Thanh Dat <datgvtdev03@gmail.com>
2022-12-05 21:46:05 +0700

    assa

--- grab.js
+++ grab.js
@@ -1,6 +1,6 @@
 //b1:import vao thu vien express
 const express = require('express')
-//b1 import thu vien msql2
+    //b1 import thu vien msql2
 const mysql2 = require('mysql2')
 
 //lien quan den insert into
@@ -19,10 +19,10 @@ const port = 3000
 
 //b2 tao databbase
 const connection = mysql2.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'Grab'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'Grab'
 });
 
 
@@ -31,83 +31,89 @@ const connection = mysql2.createConnection({
 
 //b4: tao api phuong thuc GET: localhost:3000
 app.get('/', (req, res) => {
-  res.send('<h1>Xin chao ban</h1>')
+    res.send('<h1>Xin chao ban</h1>')
 })
 
 app.get('/tinh', (req, res) => {
-  connection.connect(() => {
-    connection.query("SELECT * FROM tinh", (err, ketQuaTinh) => {
-      res.send(ketQuaTinh)
+    connection.connect(() => {
+        connection.query("SELECT * FROM tinh", (err, ketQuaTinh) => {
+            res.send(ketQuaTinh)
+        })
     })
-  })
 })
 
 app.post('/tinh', (req, res) => {
-  connection.connect(() => {
-    connection.query("INSERT INTO tinh (ma_tinh, ten_tinh) VALUES (?)", [[req.body.ma_tinh, req.body.ten_tinh]], (err) => {
-      console.log(err)
-      res.send("Them thanh cong")
+    connection.connect(() => {
+        connection.query("INSERT INTO tinh (ma_tinh, ten_tinh) VALUES (?)", [
+            [req.body.ma_tinh, req.body.ten_tinh]
+        ], (err) => {
+            console.log(err)
+            res.send("Them thanh cong")
+        })
     })
-  })
 })
 
 //dat chuyen
 // 1: lay ra phuong thuc thanh toan cua nguoi dang nhap vao tai khoan
 app.get('/phuongthucthanhtoan', (req, res) => {
-  connection.connect(() => {
-    connection.query("SELECT * FROM phuong_thuc_thanh_toan", (err, ketQua) => {
-      res.send(ketQua);
+    connection.connect(() => {
+        connection.query("SELECT * FROM phuong_thuc_thanh_toan", (err, ketQua) => {
+            res.send(ketQua);
+        })
     })
-  })
 })
 
 //api danh sach uu dai
 app.get('/uudai', (req, res) => {
-  connection.connect(() => {
-    connection.query("SELECT * FROM uu_dai", (err, ketQua) => {
-      res.send(ketQua);
+    connection.connect(() => {
+        connection.query("SELECT * FROM uu_dai", (err, ketQua) => {
+            res.send(ketQua);
+        })
     })
-  })
 })
 
 //danh sach dia diem
 app.get('/danhsachdiadiem', (req, res) => {
-  connection.connect(() => {
-    connection.query("SELECT ten_xa, ten_huyen, ten_tinh FROM xa INNER JOIN huyen ON xa.ma_huyen = huyen.ma_huyen INNER JOIN tinh ON huyen.ma_tinh = tinh.ma_tinh", (err, ketQua) => {
-      res.send(ketQua);
+    connection.connect(() => {
+        connection.query("SELECT ten_xa, ten_huyen, ten_tinh FROM xa INNER JOIN huyen ON xa.ma_huyen = huyen.ma_huyen INNER JOIN tinh ON huyen.ma_tinh = tinh.ma_tinh", (err, ketQua) => {
+            res.send(ketQua);
+        })
     })
-  })
 })
 
 
 //tim kiem dia chi
 app.get("/timkiem/:key", function(req, res) {
-  let key = req.params.key;
-  console.log("key day: " +key)
-  connection.connect(() => {
-      if(!key) {
-        res.send("Khong co gia tri")
-      }
-    connection.query("SELECT ten_xa, ten_huyen, ten_tinh FROM xa INNER JOIN huyen ON xa.ma_huyen = huyen.ma_huyen INNER JOIN tinh ON huyen.ma_tinh = tinh.ma_tinh WHERE ten_xa LIKE (?) ", [['%' + key + '%']], (err, kq) => {
-      console.log(err)
-        res.send(kq)
+    let key = req.params.key;
+    console.log("key day: " + key)
+    connection.connect(() => {
+        if (!key) {
+            res.send("Khong co gia tri")
+        }
+        connection.query("SELECT ten_xa, ten_huyen, ten_tinh FROM xa INNER JOIN huyen ON xa.ma_huyen = huyen.ma_huyen INNER JOIN tinh ON huyen.ma_tinh = tinh.ma_tinh WHERE ten_xa LIKE (?) ", [
+            ['%' + key + '%']
+        ], (err, kq) => {
+            console.log(err)
+            res.send(kq)
+        })
     })
-  })
 })
 
 // tim kiem uu dai 
 app.get('/timkiemuudai/:key', (req, res) => {
-  let key = req.params.key
-  console.log("key: " + key)
-  connection.connect(() => {
-    if(!key) {
-      res.send("Khong co gia tri tra ve")
-    }
-    connection.query("SELECT * FROM uu_dai WHERE ten_uu_dai LIKE (?) ", [['%' + key + '%']], (err, kq) => {
-      console.log(err)
-      res.send(kq)
+    let key = req.params.key
+    console.log("key: " + key)
+    connection.connect(() => {
+        if (!key) {
+            res.send("Khong co gia tri tra ve")
+        }
+        connection.query("SELECT * FROM uu_dai WHERE ten_uu_dai LIKE (?) ", [
+            ['%' + key + '%']
+        ], (err, kq) => {
+            console.log(err)
+            res.send(kq)
+        })
     })
-  })
 })
 
 //api dat chuyen
@@ -115,14 +121,7 @@ app.post('/datchuyen', (req, res) => {
 
 })
 
-
-
 //b5: mo cong 3000 va start sever day
 app.listen(port, () => {
-  console.log(`Dang chay cong: ${port}`)
+    console.log(`Dang chay cong: ${port}`)
 })
-
-
-
-
-
